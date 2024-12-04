c7c5120016ddfa1d6b7aead348601c729ee678d7
vatsal thakkar <vatsal247@gmail.com>
2024-12-04 17:31:08 -0600

    js update

--- foundations/javascript/javascript.js
+++ foundations/javascript/javascript.js
@@ -4,8 +4,50 @@
 // console.log(name);
 // console.log(surname);
 
+// let age = 11;
+// console.log(age);
+
+// age = 54;
+// console.log(age);
+
+// const pi = 3.4;
+// pi = 10;
+
+// console.log(pi);
+
+// console.log((4 + 6 + 9) / 77);
 // console.log(23 + 97);
 
-// console.log(4 + 6 + 9) / 77;
+// let a = 10;
+// console.log(a);
 
-let a = 10;
+// console.log(9 * a);
+
+// let b = 7 * a;
+// console.log(b);
+
+// const max = 57;
+// let actual = max - 13;
+// let percentage = actual / max;
+
+// console.log(percentage);
+
+// let a = 1,
+//   b = 1;
+
+// let c = ++a;
+// let d = b++;
+
+// const string = "The revolution will not be televised";
+// console.log(string);
+
+// const badString1 = This is a test;
+// const badString2 = 'This is a test;
+// // const badString3 = This is a test';
+
+// const name = "Chris";
+// const greeting = "Hello, ${name}";
+// console.log(greeting);
+const name = "Chris";
+const greeting = `Hello, ${name}`;
+console.log(greeting); // "Hello, Chris"
